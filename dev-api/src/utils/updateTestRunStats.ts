1210ba86c020e15b29bbd8593e9eef5a1ebfda09
Daniel Austin <danielaustin718@gmail.com>
2025-09-20 17:07:47 +0100

    Function to sync deleted test runs

--- dev-api/src/utils/updateTestRunStats.ts
+++ dev-api/src/utils/updateTestRunStats.ts
@@ -1,5 +1,38 @@
+import { Logger } from "@aws-lambda-powertools/logger";
 import { EdgeCase, TestCase, TestRunCase } from "@qa/models";
-import { ITestRunDocument } from "@qa/models/test-run";
+import { ITestRunDocument, TestRun } from "@qa/models/test-run";
+import { Types } from "mongoose";
+
+const logger = new Logger({ serviceName: 'qa-backend' });
+
+export const syncDeleted = async (planId: Types.ObjectId): Promise<ITestRunDocument[]> => {
+  const affectedTestRuns = await TestRun.find({ planId, status: { $ne: 'finished' } });
+  const activeTestRunIds = affectedTestRuns.map(tr => tr._id);
+  if (!activeTestRunIds.length) {
+    return [];
+  }
+
+  logger.info('Syncing deletes', { activeTestRunIds })
+
+  // Delete test run cases whose test case obj has being deleted
+  const testCaseIds = (await TestCase.find({ planId })).map(tc => tc._id);
+  // Add test plan id to the query so we don't delete testCases from other test plans
+  const res = await TestRunCase.deleteMany({ testRunId: { $in: activeTestRunIds }, testCaseId: { $not: { $in: testCaseIds } } });
+  logger.info('[On feature save]: Deleted test run cases that no longer have test cases', { planId, testCaseId: { $not: { $in: testCaseIds } }, deleted: res });
+
+  // Delete test run edge cases whose edge obj has being deleted
+  let refreshedTestRunCases = await TestRunCase.find({ testRunId: { $in: activeTestRunIds } }).populate('testCaseId', 'testRunId');
+  for (let trc of refreshedTestRunCases) {
+    const edgeCaseIds = (await EdgeCase.find({ testCase: trc.testCaseId })).map(ec => ec._id) as unknown as Types.ObjectId[];
+    trc.edgeCases = trc.edgeCases.filter(rEdgeCase =>
+      edgeCaseIds.find(id => id.equals(rEdgeCase.edgeCaseId))
+    );
+    await trc.save();
+    logger.info('[On feature save]: Cleaned up edge cases on test run case', { testRunCaseId: trc._id,  });
+  }
+
+  return affectedTestRuns;
+};
 
 export const calTestRunStats = async (testRun: ITestRunDocument): Promise<ITestRunDocument> => {
   if (!testRun) {
@@ -44,21 +77,24 @@ export const calTestRunStats = async (testRun: ITestRunDocument): Promise<ITestR
       }
     }
   ];
-  const res = (await TestRunCase.aggregate(pipeline).exec())[0];
+  const result = await TestRunCase.aggregate(pipeline).exec();
+  logger.info('Stat aggregates retrieved', { result });
 
-  const testCases = await TestCase.find({ planId: testRun.planId });
+  const testCaseIds = (await TestCase.find({ planId: testRun.planId })).map(tc => tc._id);
+
   const totalEdgeCases = await EdgeCase.countDocuments({ 
-    _id: { $in: testCases.map(tc => tc._id) }
+    testCase: { $in: testCaseIds }
   });
+  logger.info('Fetched total edgecases', { testCaseIds, totalEdgeCases });
 
   await testRun.updateOne({
     $set: {
-      'stat.totalPassed': res.stat.passed ?? 0,
-      'stat.totalFailed': res.stat.failed ?? 0,
-      'stat.totalBlocked': res.stat.blocked ?? 0,
-      'stat.totalNeedsARetest': res.stat['needs-a-retest'] ?? 0,
-      'stat.totalPassedWithWarnings': res.stat['passed-with-warnings'] ?? 0,
-      'stat.totalRun': res.total ?? 0,
+      'stat.totalPassed': result[0]?.stat?.passed ?? 0,
+      'stat.totalFailed': result[0]?.stat?.failed ?? 0,
+      'stat.totalBlocked': result[0]?.stat?.blocked ?? 0,
+      'stat.totalNeedsARetest': result[0]?.stat?.['needs-a-retest'] ?? 0,
+      'stat.totalPassedWithWarnings': result[0]?.stat?.['passed-with-warnings'] ?? 0,
+      'stat.totalRun': result[0]?.total ?? 0,
       'stat.totalEdgeCases': totalEdgeCases
     }
   });

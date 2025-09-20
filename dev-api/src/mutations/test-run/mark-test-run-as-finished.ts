184dc9ef91435e0d82f5e7c9d2b605e6e4200073
Daniel Austin <danielaustin718@gmail.com>
2025-09-20 12:19:10 +0100

    Send 0 instead of undefined

--- dev-api/src/mutations/test-run/mark-test-run-as-finished.ts
+++ dev-api/src/mutations/test-run/mark-test-run-as-finished.ts
@@ -2,7 +2,7 @@ import { TestRunCase, TestRun, TestPlan } from "@qa/models";
 import { ITestRunstat } from "@qa/models/test-run";
 import { Types } from "mongoose";
 
-export const updateTestRunCaseStats = async (_id: Types.ObjectId): Promise<ITestRunstat> => {
+export const updateTestRunCaseStats = async (_id: Types.ObjectId): Promise<Omit<ITestRunstat, 'totalEdgeCases'>> => {
   const pipeline = [
     {
       $match: {
@@ -45,12 +45,12 @@ export const updateTestRunCaseStats = async (_id: Types.ObjectId): Promise<ITest
 
   const res = edgeCases[0];
   return {
-    totalPassed: res.stat.passed,
-    totalFailed: res.stat.failed,
-    totalBlocked: res.stat.blocked,
-    totalNeedsARetest: res.stat['needs-a-retest'],
-    totalPassedWithWarnings: res.stat['passed-with-warnings'],
-    totalRun: res.total
+    totalPassed: res.stat.passed ?? 0,
+    totalFailed: res.stat.failed ?? 0,
+    totalBlocked: res.stat.blocked ?? 0,
+    totalNeedsARetest: res.stat['needs-a-retest'] ?? 0,
+    totalPassedWithWarnings: res.stat['passed-with-warnings'] ?? 0,
+    totalRun: res.total ?? 0
   };
 
 }

c543037871d0a2cad6af62b8bf8dc0ff7b197ec1
Daniel Austin <danielaustin718@gmail.com>
2025-09-21 11:02:15 +0100

    New edge case service

---
+++ dev-api/src/services/edge-case.ts
@@ -0,0 +1,30 @@
+import { EdgeCase, IEdgeCase } from "@qa/models";
+import { Types } from "mongoose";
+import { CodeGeneratorService } from "./code-generator";
+
+export class EdgeCaseService {
+    public static async createEdgeCases(
+        projectId: Types.ObjectId,
+        userId: Types.ObjectId,
+        planId: string,
+        testCaseId: string,
+        edgeCases: Pick<IEdgeCase, 'title' | 'expectation' | 'order'>[]
+    ): Promise<Types.ObjectId[]> {
+        const ids: Types.ObjectId[] = [];
+        for (let ec of edgeCases) {
+            const edgeCaseCode = await CodeGeneratorService.generateCode('EC', projectId);
+
+            const edgeCase = await EdgeCase.create({
+                user: userId,
+                plan: planId,
+                testCase: testCaseId,
+                code: edgeCaseCode,
+                title: ec.title,
+                expectation: ec.expectation,
+                order: ec.order
+            });
+            ids.push(edgeCase._id);
+        }
+        return ids;
+    }
+}

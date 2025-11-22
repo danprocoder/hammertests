21171fdc848cdba585f16a6cd5b20d4ddc58c7aa
Daniel Austin <danielaustin718@gmail.com>
2025-11-22 16:25:37 +0000

    Remove user session and auth

--- dev-api/src/queries/index.ts
+++ dev-api/src/queries/index.ts
@@ -3,4 +3,3 @@ export { testPlanQuery } from './test-plan';
 export { testFeatureQuery } from './test-feature';
 export { testRunQuery } from './test-run';
 export { testRunCaseQuery } from './test-run-case';
-export { userSessionQuery } from './user-session';
--- dev-api/src/queries/user-session.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '@qa/models';
-
-export const userSessionQuery = {
-  user: async (parent: any, args: any) => {
-    return await User.findOne({ _id: parent.userId });
-  }
-};

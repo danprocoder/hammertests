47c1624351fc751128a89f8f29eb63ef75a1ab73
Daniel Austin <danielaustin718@gmail.com>
2025-11-23 15:53:48 +0000

    Move to feature folder

--- dev-api/src/features/project/models/project.ts
+++ dev-api/src/features/project/models/project.ts
@@ -1,15 +1,28 @@
 import { Schema, Document, model, Types } from "mongoose";
 
 export interface IProject {
+  _id: Types.ObjectId;
   createdBy: Types.ObjectId;
   name: string;
+  description: string;
+  environments?: {
+    _id?: Types.ObjectId;
+    name: string;
+    url: string;
+  }[];
+  createdAt: Date;
 }
 
-export interface IProjectDocument extends IProject, Document<Types.ObjectId> {}
+export interface IProjectDocument extends IProject, Document<Types.ObjectId> { }
 
 export const Project = model<IProjectDocument>('Project', new Schema<IProjectDocument>({
   createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
-  name: { type: String }
+  name: { type: String },
+  description: { type: String },
+  environments: [{
+    name: { type: String },
+    url: { type: String }
+  }]
 }, { timestamps: true }), 'projects');
 
 export interface IProjectMember {
@@ -17,7 +30,7 @@ export interface IProjectMember {
   user: Types.ObjectId;
 }
 
-export interface IProjectMemberDocument extends IProjectMember, Document {}
+export interface IProjectMemberDocument extends IProjectMember, Document<Types.ObjectId> { }
 
 export const ProjectMember = model<IProjectMemberDocument>('ProjectMember', new Schema<IProjectMemberDocument>({
   project: { type: Schema.Types.ObjectId, ref: 'Project' },
--- dev-api/src/features/user/models/user.ts
+++ dev-api/src/features/user/models/user.ts
@@ -1,10 +1,11 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 
 interface IUser {
-  firstName: string,
-  lastName: string,
+  project: Types.ObjectId;
+  firstName?: string,
+  lastName?: string,
   email: string,
-  picture: string,
+  picture?: string,
   cognitoUsername: string,
   createdAt: any,
   updatedAt: any
@@ -15,11 +16,16 @@ interface IUserDocument extends IUser, Document<Types.ObjectId> { }
 interface IUserModel extends mongoose.Model<IUserDocument> { }
 
 const userSchema = new Schema<IUserDocument>({
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  },
   firstName: { type: String },
   lastName: { type: String },
-  email: { type: String },
+  email: { type: String, required: true },
   picture: { type: String },
-  cognitoUsername: { type: String },
+  cognitoUsername: { type: String, required: true },
 }, { timestamps: true });
 
 const User = mongoose.model<IUserDocument, IUserModel>('User', userSchema, 'users');
